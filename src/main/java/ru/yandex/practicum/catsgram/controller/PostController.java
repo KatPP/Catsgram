b1a972f968fe29adb503c90d5a4335b4044120d4
zenit1925 <kostia-85-85@yandex.ru>
2025-08-26 15:08:48 +0300

    2. Разное

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.SortOrder;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 
 @RestController
@@ -16,11 +19,29 @@ public class PostController {
         this.postService = postService;
     }
 
+    /**
+     * Получает список всех постов с параметрами сортировки и пагинации
+     *
+     * @param sort  порядок сортировки: "asc"/"ascending" или "desc"/"descending"
+     * @param from  с какого поста начинать (по умолчанию 0)
+     * @param size  сколько постов возвращать (по умолчанию 10)
+     * @return коллекция постов
+     *
+     * Примеры запросов:
+     * GET /posts                     - по умолчанию: последние 10 постов
+     * GET /posts?sort=asc&from=5&size=3  - первые 3 поста, начиная с 5-го
+     */
     @GetMapping
-    public Collection<Post> findAll() {
-        return postService.findAll();
+    public Collection<Post> findAll(
+            @RequestParam(defaultValue = "desc") String sort,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size) {
+
+        // Преобразуем строку в enum и вызываем сервисный метод
+        return postService.findAll(SortOrder.from(sort), from, size);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Post create(@RequestBody Post post) {
         return postService.create(post);
@@ -29,5 +50,10 @@ public class PostController {
     @PutMapping
     public Post update(@RequestBody Post newPost) {
         return postService.update(newPost);
+    }
+
+    @GetMapping("/{postId}")
+    public Optional<Post> findById(@PathVariable long postId) {
+        return postService.findByPostId(postId);
     }
 }
